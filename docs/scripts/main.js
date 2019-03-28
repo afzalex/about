3a95a2448d48027ffe615efd999c6bc5a5cbfea4
Mohammad Afzal <afzalex.store@gmail.com>
2019-03-28 11:38:47 +0530

    Add Heroku generated app.json

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -4,3 +4,13 @@
     gtag('js', new Date());
     gtag('config', 'UA-136552115-1');
 })();
+
+(function () {
+    $(document).ready(function(){
+        if(location.href.indexOf('github') > 0) {
+            $('.LI-profile-badge').addClass('show').removeClass('hide');
+            $('.profile-img').addClass('hide').removeClass('show');
+            $('.hidden-msg').addClass('show').removeClass('hide');
+        }
+    });
+})();
