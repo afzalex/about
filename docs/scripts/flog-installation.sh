0b82b20cda79cf4eb3c83d8304899258a883d0aa
Mohammad Afzal <mafzal@altimetrik.com>
2021-08-17 11:21:46 +0530

    flog-installation.sh added

---
+++ docs/scripts/flog-installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+GITCONFIG_LOC="$HOME/.gitconfig"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+if ! cat "$GITCONFIG_LOC" | grep '#### FZ_CODE_FLOG_INSTALLATION ####' > /dev/null
+then 
+cat <<END >> $GITCONFIG_LOC
+
+#### FZ_CODE_FLOG_INSTALLATION ####
+[alias]
+    flog = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
+END
+    printf "${GREEN}flog installed successfully${NC}\n"
+else
+    printf "${GREEN}flog is already installed${NC}\n"
+fi
