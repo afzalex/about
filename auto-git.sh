c53af4b1dbb523daed60f97d3a9f6b37c5c70120
Mohammad Afzal <mafzal@calibo.com>
2022-06-14 21:18:00 +0530

    added song

--- auto-git.sh
+++ auto-git.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/expect -f
 
-set argsCount [llength $argv]
-lassign $argv arg1 arg2 arg3
-
 set timeout 15
+set argsCount [llength $argv]
+lassign $argv arg1 arg2 arg3 arg4 arg5
 
 if { $argsCount == 1} {
     spawn git $arg1 
@@ -11,8 +10,12 @@ if { $argsCount == 1} {
     spawn git $arg1 $arg2
 } elseif { $argsCount == 3} {
     spawn git $arg1 $arg2 $arg3
+} elseif { $argsCount == 4} {
+    spawn git $arg1 $arg2 $arg3 $arg4
+} elseif { $argsCount == 5} {
+    spawn git $arg1 $arg2 $arg3 $arg4 $arg5
 } else {
-    send_user "expected at least 1 argument\n"
+    send_user "expected at least 1 and at most 5 arguments\n"
     exit
 }
 
